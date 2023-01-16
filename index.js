935fed038239ccbfaa48cfa1e291ee12da61644c
ajeer333 <ajeer333@gmail.com>
2023-01-16 23:31:54 +0100

    commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-
-const getPinkfloydSongs=()=>{
-
-
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=pink floyd",{method:"GET" ,headers: {
+let songs=[];
+const options = {
+	method: 'GET',
+	headers: {
 		'X-RapidAPI-Key': '1a0e486a8emshc09f82e64512603p1d21fbjsnd674ceee0cc4',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'}
-	})
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
+
+
+const getSongs=(search,id)=>{
+
+
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q="+search ,options)
 
     .then((rawsongs)=>{
         return rawsongs.json()
@@ -13,15 +19,18 @@ const getPinkfloydSongs=()=>{
         
         let songsObject=jsonSongs.data
         console.log(songsObject)
-        let ulNode=document.getElementById("first");
+        let place=document.getElementById(id);
        
         for(let i=0;i<songsObject.length;i++){
             let singleSong=songsObject[i];
-            ulNode.innerHTML+=`  
+            songs.push(singleSong)
+           
+            place.innerHTML+=`  
             
-            <div class="col-6 col-md-3 ">
-            <div class="card" ">
+            <div class="col-6 col-md-3 mb-3 ">
+            <div class="card">
             <img src="${singleSong.album.cover_medium}" class="card-img-top img-fluid" alt="...">
+            
             <div class="card-body">
               <p class="card-text">${singleSong.title}</p>
             </div>
@@ -33,133 +42,100 @@ const getPinkfloydSongs=()=>{
     }).catch(err=>console.log(err))
 
 }
+window.onload= function(){
+getSongs("pink floyd","first")
 
+getSongs("daft punk","second")
+getSongs("metallica","third")
+}
 
-const getdaftpunkSongs=()=>{
+ console.log(songs)
 
-
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=daft punk",{method:"GET" ,headers: {
-		'X-RapidAPI-Key': '1a0e486a8emshc09f82e64512603p1d21fbjsnd674ceee0cc4',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'}
-	})
-
-    .then((rawsongs)=>{
-        return rawsongs.json()
-    }).then((jsonSongs)=>{
+function uniqueCounter(){
+    let btn=document.getElementById("countunique")
+    btn.addEventListener("click",function(){
+        let array=[]
+        for(i=0;i<songs.length;i++){
+            let song=songs[i]
+            // console.log(song.album.title)
+            if(!array.includes(song.album.id)){
+                
+                array.push(song.album.id)
+            }
+        }
+        console.log(array.length)
+    
         
-        let songsObject=jsonSongs.data
-        console.log(songsObject)
-        let ulNode=document.getElementById("second");
+    })
+}
+
+
+uniqueCounter()
+
+function songList(){
+    let btn=document.getElementById('songlist')
+    let where=document.getElementById('modalbody')
+   
+    btn.addEventListener("click",function(){
+        for(i=0;i<songs.length;i++){
+                 let song=songs[i];
+                 where.innerHTML+=`<div class="text-left" style="border:1px solid black">
+                 <span>${i+1}</span>
+                 <span>${song.title_short}</span>
+                 </div>`
+              
+        }
+    })
+}
+songList();
+
+
+
+
+// function valuefunction(eventData){
+//     if(eventData.key==="Enter"){
+//       let val=document.getElementById("inp").value
+//     }
+//     return val
+//    }
+
+
+// function searchBar(){
+
+// search=valuefunction()
+// console.log(search)
+
       
-        for(let i=0;i<songsObject.length;i++){
-            let singleSong=songsObject[i];
-            ulNode.innerHTML+=` 
-            
-            <div class="col-6 col-md-3">
-            <div class="card" >
-            <img src="${singleSong.album.cover_medium}" class="card-img-top img-fluid" alt="...">
-            <div class="card-body">
-              <p class="card-text">${singleSong.title}</p>
-            </div>
-            </div>
-          
-          </div>`
-        }
-
-    }).catch(err=>console.log(err))
-
-}
-
-
-
-const getMetallicaSongs=()=>{
-
-
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica",{method:"GET" ,headers: {
-		'X-RapidAPI-Key': '1a0e486a8emshc09f82e64512603p1d21fbjsnd674ceee0cc4',
-		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'}
-	})
-
-    .then((rawsongs)=>{
-        return rawsongs.json()
-    }).then((jsonSongs)=>{
-        
-        let songsObject=jsonSongs.data
-        console.log(songsObject)
-        let ulNode=document.getElementById("third");
-        
-        for(let i=0;i<songsObject.length;i++){
-            let singleSong=songsObject[i];
-            ulNode.innerHTML+=` 
-           
-            <div class="col-6 col-md-3">
-            <div class="card" >
-            <img src="${singleSong.album.cover_medium}" class="card-img-top img-fluid" alt="...">
-            <div class="card-body">
-              <p class="card-text">${singleSong.title}</p>
-            </div>
-         
-            </div>
-          </div>`
-        }
-
-    }).catch(err=>console.log(err))
-
-}
-// function uniqueCount(){
-//     let btn=document.getElementById('countunique')
-//     let unique=[]
-//     btn.addEventListener("click",function(){
-//         console.log("metallica's unique albums")
-//         fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=metallica",{method:"GET" ,headers: {
-//             'X-RapidAPI-Key': '1a0e486a8emshc09f82e64512603p1d21fbjsnd674ceee0cc4',
-//             'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'}
-//         })
+//         fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q="+search ,options)
+    
 //         .then((rawsongs)=>{
 //             return rawsongs.json()
 //         }).then((jsonSongs)=>{
             
 //             let songsObject=jsonSongs.data
-            
+//             console.log(songsObject)
+//             let place=document.getElementById("seachresult");
+           
 //             for(let i=0;i<songsObject.length;i++){
 //                 let singleSong=songsObject[i];
-//                 console.log(singleSong)
-//             }
               
+               
+//                 place.innerHTML+=`  
+                
+//                 <div class="col-6 col-md-3 mb-3 ">
+//                 <div class="card">
+//                 <img src="${singleSong.album.cover_medium}" class="card-img-top img-fluid" alt="...">
+                
+//                 <div class="card-body">
+//                   <p class="card-text">${singleSong.title}</p>
+//                 </div>
+               
+//                 </div>
+//               </div>`
+//             }
+    
 //         }).catch(err=>console.log(err))
-
-//     })
+    
+    
 // }
-// uniqueCount()
-
-
-
-   
-      
-
-
-function songList(){
-    let btn=document.getElementById('songlist')
-    let where=document.getElementById('modalbody')
-    btn.addEventListener("click",function(){
-       let ps=document.getElementsByTagName('p')
-       for(let i=0;i<ps.length;i++){
-        let p=ps[i];
-        modalbody.innerText+=p.innerText
-        modalbody.innerHTML+=`<br>`
-   
-       }
-
-    })
-}
-songList()
-
-
-
-
-
-window.onload= function(){
-    getPinkfloydSongs();
-    getdaftpunkSongs();
-    getMetallicaSongs();
-}
+// searchBar();
